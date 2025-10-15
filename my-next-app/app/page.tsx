6921694c16e57c7c2da2eb223644990cc6be8dfb
MizAccessWork <muhammadmizanur.rahman@bramptonmanor.org.uk>
2025-10-15 14:22:34 +0100

    newchc

--- my-next-app/app/page.tsx
+++ my-next-app/app/page.tsx
@@ -95,7 +95,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.orfsfdskljlkdfsdfg →
+          Go to nextjs.orfsfdskldsffsdjlkdfsdfg →
         </a>
       </footer>
     </div>
